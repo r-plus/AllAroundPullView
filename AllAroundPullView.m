5813a5feb94d40625b98c460b2e8ea935f3ca7ca
r-plus <anasthasia.r@gmail.com>
2012-09-09 00:09:38 +0900

    for byline tweak

--- AllAroundPullView.m
+++ AllAroundPullView.m
@@ -98,7 +98,7 @@ static const CGFloat kSidePullViewWidth = 60.0f;
         self.backgroundColor = [UIColor clearColor];
         self.threshold = 60.0f;
         self.arrowImage = [[[CALayer alloc] init] autorelease];
-        UIImage *arrow = [UIImage imageNamed:@"arrow.png"];
+        UIImage *arrow = [UIImage imageWithContentsOfFile:@"/Library/Application Support/BylineEnhancer/arrow.png"];
         self.arrowImage.contents = (id) arrow.CGImage;
         CGRect arrowAndActivityFrame;
         if (self.isSideView) {
